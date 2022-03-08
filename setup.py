9ffe28abf69040b55d6f34a4b73d4280ad4f8442
Daniel H <54770658+danielhamen@users.noreply.github.com>
2022-03-08 13:11:52 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+
+setup(
+    name = "Break",
+    packages = ["Break"],
+    version = "1.0.0",
+    license = "GNU General Public License v3.0",
+    description = "Adds a \"br()\" function to Python which print() or return a certain amount of newlines",
+    author = "Daniel Hamen",
+    author_email = "danielhamen92@gmail.com",
+    url = "https://github.com/danielhamen",
+    download_url = "https://github.com/user/reponame/archive/v_01.tar.gz",
+    keywords = ["SOME", "MEANINGFULL", "KEYWORDS"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: GNU General Public License v3.0",
+        "Programming Language :: Python :: 3.0",
+        "Programming Language :: Python :: 3.1",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10"
+    ],
+)
